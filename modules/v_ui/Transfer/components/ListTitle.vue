7b03b7f586033299171be343d942b13d5ca88814
spurs0929 <briankang0929@gmail.com>
2022-08-25 20:56:47 +0800

    拆分ListTitle組件

---
+++ modules/v_ui/Transfer/components/ListTitle.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1 class="list-title">{{ title }}</h1>
+</template>
+
+<script setup>
+  const props = defineProps({
+    title: {
+      type: String, 
+      default: '列表'
+    }
+  })
+</script>
+
+<style lang="scss" scoped>
+  .list-title{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 38px;
+    font-weight: normal;
+    margin: 0;
+    color: #000;
+    background-color: #f3eded;
+    font-size: 14px;
+  }
+</style>
